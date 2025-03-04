20ea3dd90438390e3470f60b6bcdb6a171331c9f
jeffschoe <ratacame@protonmail.com>
2025-03-04 00:01:06 -0500

    Added function to get quantity of words in the text, Bookbot module Ch2 L4

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ def get_book_text(path_to_file):
         file_contents = f.read() #.read() method puts content into a string
     return file_contents
 
+def num_words(book_text):
+    return len(book_text.split())
+
 def main():
-    print(f"{get_book_text("./books/frankenstein.txt")}")
+    #print(f"{get_book_text("./books/frankenstein.txt")}")
+    print(f"{num_words(get_book_text("./books/frankenstein.txt"))} words found in the document")
     return
 
 main()
