614efbe3fbcaec86a01fa96b70beea03131a2f47
jeffschoe <ratacame@protonmail.com>
2025-03-04 20:33:29 -0500

    Refactored code to include a stat.py file. Renamed num_words function to get_num_words. CH2 L5.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f: #with block opens a file
         file_contents = f.read() #.read() method puts content into a string
     return file_contents
 
-def num_words(book_text):
-    return len(book_text.split())
-
 def main():
     #print(f"{get_book_text("./books/frankenstein.txt")}")
-    print(f"{num_words(get_book_text("./books/frankenstein.txt"))} words found in the document")
+    print(f"{get_num_words(get_book_text("./books/frankenstein.txt"))} words found in the document")
     return
 
 main()
