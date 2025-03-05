94ce7d3cfe7ee8504b115aae625d8fabc44b1021
jeffschoe <ratacame@protonmail.com>
2025-03-04 21:35:06 -0500

    Added new function to count the number of unique characters in the book. CH2 L6.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f: #with block opens a file
@@ -8,6 +9,9 @@ def get_book_text(path_to_file):
 def main():
     #print(f"{get_book_text("./books/frankenstein.txt")}")
     print(f"{get_num_words(get_book_text("./books/frankenstein.txt"))} words found in the document")
+    count_of_chars = get_num_chars(get_book_text("./books/frankenstein.txt"))
+    print(count_of_chars)
+    
     return
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def get_num_words(book_text):
     return len(book_text.split())
+
+def get_num_chars(book_text):
+    lower_book_text = book_text.lower()
+    total_chars_and_counts = {}
+    for char in lower_book_text:
+        if char in total_chars_and_counts:
+            total_chars_and_counts[char] += 1
+        else: 
+            total_chars_and_counts[char] = 1
+    return total_chars_and_counts
