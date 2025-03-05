a093135aebc324c25a61040dab97a1c5f53d5cc4
jeffschoe <ratacame@protonmail.com>
2025-03-05 17:48:22 -0500

    Completed report section, CH3 L1.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import sort_chars_and_counts
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f: #with block opens a file
@@ -8,10 +9,28 @@ def get_book_text(path_to_file):
 
 def main():
     #print(f"{get_book_text("./books/frankenstein.txt")}")
-    print(f"{get_num_words(get_book_text("./books/frankenstein.txt"))} words found in the document")
-    count_of_chars = get_num_chars(get_book_text("./books/frankenstein.txt"))
-    print(count_of_chars)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at TBD")
+
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(get_book_text("./books/frankenstein.txt"))} total words")
     
+    print("--------- Character Count -------")
+    count_of_chars = get_num_chars(get_book_text("./books/frankenstein.txt"))
+    #print(count_of_chars) prints unsorted dictionary
+    sorted_chars_and_counts = sort_chars_and_counts(count_of_chars) #list of dictionaries
+    #print(sorted_chars_and_counts) prints sorted list of dictionaries
+    i = 0
+    while i < len(sorted_chars_and_counts):
+        if sorted_chars_and_counts[i]["character"].isalpha():
+            #print(sorted_chars_and_counts[i])
+            print(f"{sorted_chars_and_counts[i]["character"]}: {sorted_chars_and_counts[i]["count"]}")
+            i +=1
+        else:
+            i += 1
+
+    print("============= END ===============")
+
     return
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,18 @@ def get_num_chars(book_text):
         else: 
             total_chars_and_counts[char] = 1
     return total_chars_and_counts
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_chars_and_counts(chars_and_counts_dict):
+    unsorted_dicts_list = []
+    sorted_dicts_list = []
+    for char_and_count in chars_and_counts_dict:
+        temp_dict = {"character": char_and_count, "count": chars_and_counts_dict[char_and_count]}
+        unsorted_dicts_list.append(temp_dict)
+        #now have list of dicts, but need to sort it
+        unsorted_dicts_list.sort(reverse=True, key=sort_on)
+        sorted_dicts_list = unsorted_dicts_list
+        #sort_on part is not working yet.
+    return sorted_dicts_list
